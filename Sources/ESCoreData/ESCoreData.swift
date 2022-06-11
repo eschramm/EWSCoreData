3940c5d8ffbe6b9127a28f5527d4131beedfbfa8
Eric Schramm <eschramm@mac.com>
2022-06-10 20:05:03 -0500

    Initial Commit

---
+++ Sources/ESCoreData/ESCoreData.swift
@@ -0,0 +1,6 @@
+public struct ESCoreData {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ESCoreDataTests/ESCoreDataTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ESCoreData
+
+final class ESCoreDataTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ESCoreData().text, "Hello, World!")
+    }
+}
